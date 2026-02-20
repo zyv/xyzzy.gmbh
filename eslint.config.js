d1bd17a2fd4f4ac3d41878f48fa206b21a449618
Yury V. Zaytsev <yury@shurup.com>
2026-02-20 08:07:21 +0100

    chore(elint): make config typed and follow the recommended standard

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,25 @@
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import html from 'eslint-plugin-html';
 
-export default [
-  js.configs.recommended,
+export default defineConfig([
   {
+    files: ['**/*.js'],
+    plugins: {
+      js
+    },
+    extends: ['js/recommended']
+  },
+  {
+    files: ['**/*.html'],
+    plugins: {
+      html: html
+    },
     languageOptions: {
       ecmaVersion: 6,
       sourceType: 'module',
       globals: globals.browser
-    },
-    plugins: {
-      html: html
     }
   }
-];
+]);
